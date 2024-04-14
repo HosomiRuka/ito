c8258c80aca90dfe04a745cc98f4c191c1aa0b05
HosomiRuka <2002rukahosomi@gmail.com>
2024-04-14 21:18:33 +0900

    a

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 const cardWrap = document.querySelector(".card-wrap");
 const btn = document.querySelector("#btn");
 const element = document.createElement("div");
+const cards = document.createElementAll(".card");
+
+cards.forEach((card) => {
+  console.log(card);
+});
+
+console.log("OK");
