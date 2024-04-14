abeb7389bdb88b2e10e3a1cd66a8203294210832
HosomiRuka <2002rukahosomi@gmail.com>
2024-04-15 01:57:48 +0900

    違う...

--- main.js
+++ main.js
@@ -6,16 +6,16 @@
 
 const minus = document.getElementById("minus");
 const plus = document.getElementById("plus");
-const number = document.getElementById("number");
+const createCardNumber = document.getElementById("number");
 const cardWrap = document.querySelector(".card-wrap");
-const newBtn = document.querySelector("#new");
+const newCardCreateBtn = document.querySelector("#new");
 const front = document.querySelector(".front");
-const card = document.querySelector(".card");
+const cards = document.querySelectorAll(".card");
 const decisionBtn = document.querySelector("#decision");
 
 // 読み込まれた時に1以下ならボタンを押せないようにする
 window.addEventListener("load", () => {
-  if (number.value <= 1) {
+  if (createCardNumber.value <= 1) {
     minus.style.pointerEvents = "none";
     minus.style.opacity = "0.5";
   }
@@ -24,7 +24,7 @@ window.addEventListener("load", () => {
 // -を押した時2以下ならボタンを押せないようにする、それ以外は押せる
 // valueの中身を減らす
 minus.addEventListener("click", () => {
-  if (number.value <= 2) {
+  if (createCardNumber.value <= 2) {
     minus.style.pointerEvents = "none";
     minus.style.opacity = "0.5";
   } else {
@@ -32,31 +32,52 @@ minus.addEventListener("click", () => {
     minus.style.opacity = "1";
   }
 
-  number.value--;
+  createCardNumber.value--;
 });
 
 // +を押した時1含む1以上の数字なら-ボタンを押せるように
 // valueの値を増やす。
 plus.addEventListener("click", () => {
-  if (number.value >= 1) {
+  if (createCardNumber.value >= 1) {
     minus.style.pointerEvents = "auto";
     minus.style.opacity = "1";
   }
 
-  number.value++;
+  createCardNumber.value++;
 });
 
-newBtn.addEventListener("click", () => {
+newCardCreateBtn.addEventListener("click", () => {
+  let inputNumber = createCardNumber.value;
+
+  // inputに入ってる数分コピーを繰り替えす
+  for (let i = 0; i < inputNumber; i++) {
+    let newDivs = document.createElement("li");
+    let newFront = document.createElement("div");
+    let newBack = document.createElement("div");
+
+    newDivs.classList.add("card");
+    newBack.classList.add("back");
+    newFront.classList.add("front");
+
+    cards.forEach((card) => {
+      cardWrap.appendChild(newDivs);
+      card.appendChild(newBack);
+      card.appendChild(newFront);
+    });
+  }
+
   let random = Math.floor(Math.random() * 100) + 1;
-  console.log(random);
   front.textContent = random;
 });
 
-decisionBtn.addEventListener("click", () => {
-  card.classList.add("hidden");
-  card.classList.remove("open");
-});
+// カード開閉
+cards.forEach((card) => {
+  decisionBtn.addEventListener("click", () => {
+    card.classList.add("hidden");
+    card.classList.remove("open");
+  });
 
-card.addEventListener("click", () => {
-  card.classList.toggle("open");
+  card.addEventListener("click", () => {
+    card.classList.toggle("open");
+  });
 });
