9b73e99dacd3e852e638989a7f3b26f95ecf7c3c
HosomiRuka <2002rukahosomi@gmail.com>
2024-04-16 23:27:26 +0900

    配列を戻り値に設定

--- main.js
+++ main.js
@@ -12,6 +12,9 @@ const cardWrap = document.querySelector(".card-wrap");
 const newCardCreateBtn = document.querySelector("#new");
 const decisionBtn = document.querySelector("#decision");
 
+/*--------------------
++-ボタン
+--------------------*/
 // 読み込まれた時に1以下ならボタンを押せないようにする
 window.addEventListener("load", () => {
   if (createCardNumber.value <= 1) {
@@ -45,51 +48,9 @@ plus.addEventListener("click", () => {
   createCardNumber.value++;
 });
 
-// カードと乱数を生成するボタン
-newCardCreateBtn.addEventListener("click", () => {
-  // 1回前のカードを消す。
-  while (cardWrap.firstChild) {
-    cardWrap.removeChild(cardWrap.firstChild);
-  }
-
-  // inputの中の数字を取得
-  let inputNumber = createCardNumber.value;
-  console.log(inputNumber);
-
-  // inputに入ってる数分コピーを繰り替えす
-  for (let i = 0; i < inputNumber; i++) {
-    // htmlを生成
-    let newLi = document.createElement("li");
-    let newFront = document.createElement("div");
-    let newBack = document.createElement("div");
-
-    newLi.classList.add("card");
-    newBack.classList.add("back");
-    newFront.classList.add("front");
-
-    cardWrap.appendChild(newLi);
-    newLi.appendChild(newBack);
-    newLi.appendChild(newFront);
-  }
-
-  // ランダムな数字を生成する
-  let random = Math.floor(Math.random() * 100) + 1;
-  let backText = "?";
-
-  console.log(backText);
-
-  const fronts = document.querySelectorAll(".front");
-  const backs = document.querySelectorAll(".back");
-  fronts.forEach((front) => {
-    front.textContent = random;
-  });
-
-  backs.forEach((back) => {
-    back.textContent = backText;
-  });
-});
-
-// カード開閉
+/*--------------------
+これで決定！ボタン
+--------------------*/
 decisionBtn.addEventListener("click", () => {
   // 一番上に書くと、読み込まれた直後に要素を探しにいくから、今回みたいに生成されたものを取得してくれない。
   // 読み込まれた時 → ない
@@ -107,3 +68,102 @@ decisionBtn.addEventListener("click", () => {
     });
   });
 });
+
+/*--------------------
+数字を生成するボタン
+--------------------*/
+// 乱数を指定の個数分生成する関数
+function randomCreate() {
+  let inputNumber = createCardNumber.value;
+  console.log(inputNumber + "：インプットの中身");
+
+  let randomNumber = [];
+  console.log(randomNumber);
+
+  // ランダムな入力された数だけ数字を生成する
+  for (let i = 0; i < inputNumber; i++) {
+    let random = Math.floor(Math.random() * 100) + 1;
+    console.log(random + "：ランダムな数");
+
+    randomNumber.push(random);
+    console.log(randomNumber + "：配列格納後");
+  }
+  //returnにすると配列じゃなくなる？？？？？？？？forの中にいれとったわ
+  return randomNumber;
+}
+
+// 生成したランダムな数字をカードに表示させる
+function cardOutput(randomNumber) {
+  let backText = "?";
+  const fronts = document.querySelectorAll(".front");
+  const backs = document.querySelectorAll(".back");
+
+  fronts.forEach((front) => {
+    front.textContent = randomNumber;
+  });
+  backs.forEach((back) => {
+    back.textContent = backText;
+  });
+}
+
+// カードを生成するボタンを押して上記二つを発火させる
+newCardCreateBtn.addEventListener("click", () => {
+  // きっと関数とかにまとめて書いたほうがいいんだろうけど、書き方がわからないのでまとめて書きます。
+
+  // 1回前のカードを消す。
+  while (cardWrap.firstChild) {
+    cardWrap.removeChild(cardWrap.firstChild);
+  }
+
+  let inputNumber = createCardNumber.value;
+  // inputに入ってる数分コピーを繰り替えす
+  for (let i = 0; i < inputNumber; i++) {
+    // htmlを生成
+    let newLi = document.createElement("li");
+    let newFront = document.createElement("div");
+    let newBack = document.createElement("div");
+
+    newLi.classList.add("card");
+    newBack.classList.add("back");
+    newFront.classList.add("front");
+
+    cardWrap.appendChild(newLi);
+    newLi.appendChild(newBack);
+    newLi.appendChild(newFront);
+  }
+
+  // ランダムな入力された数だけ数字を生成する
+  // for (let i = 0; i < inputNumber; i++) {
+  //   let random = Math.floor(Math.random() * 100) + 1;
+  //   const fronts = document.querySelectorAll(".front");
+
+  //   // 数字をカードに表示させる
+  //   fronts.forEach((front) => {
+  //     front.textContent = random;
+  //   });
+
+  //   console.log(random);
+  // }
+
+  let randomNumber = randomCreate();
+  cardOutput(randomNumber);
+
+  let backText = "?";
+  const backs = document.querySelectorAll(".back");
+
+  backs.forEach((back) => {
+    back.textContent = backText;
+  });
+});
+
+// 戻り値練習
+// function test() {
+//   let a = "aです";
+
+//   return a;
+// }
+
+// decisionBtn.addEventListener("click", () => {
+//   let a = test();
+//   console.log(a + "よね？");
+// });
